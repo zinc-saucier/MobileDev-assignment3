6e943278c3bd30e68dbe6930353751eebf824836
J-Wales <151964658+J-Wales@users.noreply.github.com>
2026-03-10 15:20:53 -0600

    First commit

--- app/(tab)/profile/index.tsx
+++ app/(tab)/profile/index.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const profileSchema = z.object({
+  firstName: z
+    .string()
+    .trim()
+    .min(2, "First name must be at least 2 characters."),
+  lastName: z
+    .string()
+    .trim()
+    .min(2, "Last name must be at least 2 characters."),
+  email: z.string().trim().email("Must be a valid email address."),
+  phone: z
+    .string()
+    .refine(
+      (val) => val.replace(/\D/g, "").length >= 10,
+      "Phone number must be at least 10 digits.",
+    ),
+});
 
 //the large 5 field form goes here!
 const index = () => {
