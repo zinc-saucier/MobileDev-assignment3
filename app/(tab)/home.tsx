fe9ab29e6a19c584f5eefe21218f737c3efa03d6
Tess Unrau <70186751+zinc-saucier@users.noreply.github.com>
2026-04-17 22:14:36 -0600

    ??

--- app/(tab)/home.tsx
+++ app/(tab)/home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import {theme} from "../../styles/theme";
+import {theme} from "@/styles/theme";
 
 const home = () => {
   return (
